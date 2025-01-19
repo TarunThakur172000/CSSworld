45d3bb93661f1fb0b7947a7572c7c6159d87d3a1
Tarun Thakur <tarunt5555@gmail.com>
2025-01-19 16:46:36 +0530

    sitemap update

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
+// All URLs on your site
 const urls = [
   { url: "/", lastmod: "2025-01-01" },
   { url: "/gradient-generator", lastmod: new Date().toISOString() },
   { url: "/box-shadow", lastmod: new Date().toISOString() },
+  { url: "/text-shadow", lastmod: new Date().toISOString() },
+  { url: "/css-cursor", lastmod: new Date().toISOString() },
+  { url: "/border", lastmod: new Date().toISOString() },
+  { url: "/gradient-css", lastmod: new Date().toISOString() },
+  { url: "/transform-css", lastmod: new Date().toISOString() },
+  { url: "/rgba-and-hex-color", lastmod: new Date().toISOString() },
+  { url: "/multiple-columns", lastmod: new Date().toISOString() },
+  { url: "/image-filter", lastmod: new Date().toISOString() },
+  { url: "/scrollbar", lastmod: new Date().toISOString() },
 ];
 
+// Generate sitemap XML
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${urls
@@ -20,5 +31,6 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     .join("")}
 </urlset>`;
 
+// Write sitemap to the correct location
 fs.writeFileSync(path.join(__dirname, "app", "sitemap.xml"), sitemap, "utf8");
 console.log("Sitemap generated!");
